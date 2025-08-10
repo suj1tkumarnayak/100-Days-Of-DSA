738c77ecd1691e09c2bb192828b78e4cb1731595
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2025-08-10 21:52:32 +0530

    All the practice done on 10th Aug 2025

--- src/day_033_date_date_2026_04_10/CountOccurencesOfAnagrams.java
+++ src/day_033_date_date_2026_04_10/CountOccurencesOfAnagrams.java
@@ -2,6 +2,43 @@ package day_033_date_date_2026_04_10;
 
 public class CountOccurencesOfAnagrams {
     public static void main(String[] args) {
+        String txt = "forxxorfxdofr";
+        String pat = "for";
+        System.out.println(countOccurencesOfAnagrams(txt, pat));
+    }
 
+    private static int countOccurencesOfAnagrams(String txt, String pat) {
+        int[] patCount = new int[26];
+        int[] txtCount = new int[26];
+        int len = txt.length(), k = pat.length(), count = 0;
+
+        for (char c : pat.toCharArray()) {
+            patCount[c-'a']++;
+        }
+
+        for (int i = 0; i < len; i++) {
+            txtCount[txt.charAt(i)-'a']++;
+
+            if(i>=k-1){
+                if(checkCount(txtCount, patCount)){
+                    count++;
+                }
+                txtCount[txt.charAt(i-k+1)-'a']--;
+            }
+        }
+
+        return count;
+    }
+
+    private static boolean checkCount(int[] a, int[] b) {
+        int len = a.length;
+
+        for (int i = 0; i < len; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
