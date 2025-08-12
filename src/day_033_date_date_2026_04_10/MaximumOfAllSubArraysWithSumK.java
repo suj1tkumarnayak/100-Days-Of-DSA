be94367a1adcf94526209a7df0aa51f3a587ab45
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2025-08-12 23:30:07 +0530

    all the practice done on 12th aug 2025

--- src/day_033_date_date_2026_04_10/MaximumOfAllSubArraysWithSumK.java
+++ src/day_033_date_date_2026_04_10/MaximumOfAllSubArraysWithSumK.java
@@ -1,7 +1,38 @@
 package day_033_date_date_2026_04_10;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 public class MaximumOfAllSubArraysWithSumK {
     public static void main(String[] args) {
+        List<Integer> A = List.of(1, 3, -1, -3, 5, 3, 6, 7 );
+        System.out.println(slidingMaximum(A, 3));
+    }
+    public static ArrayList<Integer> slidingMaximum(final List<Integer> A, int B){
+        ArrayList<Integer> ans = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        int len = A.size();
 
+        for (int i = 0; i < len; i++) {
+            // remove elements which are out of the window B
+            if(!dq.isEmpty() && dq.getFirst()<=i-B){
+                dq.pollFirst();
+            }
+
+            // remove elements from the last which are less than A.get(i)
+            while (!dq.isEmpty() && A.get(dq.getLast()) <= A.get(i)){
+                dq.pollLast();
+            }
+
+            dq.addLast(i);
+
+            if(!dq.isEmpty() && i>=B-1){
+                ans.add(A.get(dq.peek()));
+            }
+        }
+
+        return ans;
     }
 }
