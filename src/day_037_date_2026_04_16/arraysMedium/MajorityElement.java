d3154f5eb3326f20757b307293bb16d50148bade
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2026-04-20 10:00:00 +0530

    Practice done on 20th april 2026

---
+++ src/day_037_date_2026_04_16/arraysMedium/MajorityElement.java
@@ -0,0 +1,2 @@
+package day_037_date_2026_04_16.arraysMedium;public class MajorityElement {
+}
---
+++ src/day_037_date_2026_04_16/binarySearch/LowerBound.java
@@ -0,0 +1,22 @@
+package day_037_date_2026_04_16.binarySearch;
+
+public class LowBound {
+    public static void main(String[] args) {
+        int[] nums = { 0,1,2,2,2,3 };
+        System.out.println(lowerBound(nums, 2));
+    }
+    public static int lowerBound(int[] nums, int target){
+        int l = 0, h = nums.length;
+        int ans = -1;
+        while (l<=h){
+            int mid = l+(h-l)/2;
+            if(nums[mid]>=target){
+                ans = mid;
+                h = mid - 1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return ans;
+    }
+}
