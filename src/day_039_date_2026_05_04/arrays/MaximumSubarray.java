bf0d2946cce4f422048d482380868f5d165a7408
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2026-04-30 10:00:00 +0530

    Practice done on 30th April 2026

--- src/day_039_date_2026_05_04/arrays/MaximumSubarray.java
+++ src/day_039_date_2026_05_04/arrays/MaximumSubarray.java
@@ -1,2 +1,32 @@
-package day_039_date_2026_05_04.arrays;public class MaximumSubarray {
+package day_039_date_2026_05_04.arrays;
+
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+    public static int maxSubArrayBrute(int[] nums){
+        int max = Integer.MIN_VALUE;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int sum = nums[i];
+            max = Math.max(max, sum);
+            for (int j = i+1; j < n; j++) {
+                sum += nums[j];
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+    public static int maxSubArray(int[] nums){
+        int currSum = nums[0];
+        int currMax = nums[0];
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            currSum = Math.max(nums[i], nums[i]+currSum);
+            currMax = Math.max(currMax, currSum);
+        }
+
+        return currMax;
+    }
 }
--- src/day_039_date_2026_05_04/arrays/ProductOfArrayExceptSelf.java
+++ src/day_039_date_2026_05_04/arrays/ProductOfArrayExceptSelf.java
@@ -1,2 +1,29 @@
-package day_039_date_2026_05_04.arrays;public class ProductOfArrayExceptSelf {
+package day_039_date_2026_05_04.arrays;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(productExcept(nums)));
+    }
+    public static int[] productExcept(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        prefix[0] = 1;
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1]*nums[i-1];
+        }
+        //after finishing this {1, 1, 2, 3}
+        suffix[n-1] = 1;
+        for (int i = n-2; i >=0 ; i--) {
+            suffix[i] = suffix[i+1]*nums[i+1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = prefix[i]*suffix[i];
+        }
+        return nums;
+    }
 }
