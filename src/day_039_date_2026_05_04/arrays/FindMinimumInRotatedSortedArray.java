8b611d1bac651e3d19bf5f49b0c4b986cfdebb25
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2026-05-02 10:00:00 +0530

    Practice done on 2nd May 2026

--- src/day_039_date_2026_05_04/arrays/FindMinimumInRotatedSortedArray.java
+++ src/day_039_date_2026_05_04/arrays/FindMinimumInRotatedSortedArray.java
@@ -1,2 +1,37 @@
-package day_039_date_2026_05_04.arrays;public class FindMinimumInRotatedSortedArray {
+package day_039_date_2026_05_04.arrays;
+
+public class FindMinimumInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,3,4,5,6,7};
+        System.out.println(findMin(nums));
+    }
+
+    private static int findMin(int[] nums) {
+        int n = nums.length;
+        int min = nums[0];
+        if(n==1) return min;
+        int l = 0, h = nums.length-1;
+
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(nums[mid]<nums[l]){
+                if(nums[mid]<min){
+                    min = nums[mid];
+                }
+                h = mid-1;
+            }else{
+            }
+        }
+        return 1;
+    }
+
+    public static int findMinBrute(int[] nums){
+        int min = Integer.MAX_VALUE;
+        
+        for (int num : nums) {
+            min = Math.min(min, num);
+        }
+
+        return min;
+    }
 }
--- src/day_039_date_2026_05_04/arrays/MaximumProductSubarray.java
+++ src/day_039_date_2026_05_04/arrays/MaximumProductSubarray.java
@@ -1,2 +1,38 @@
-package day_039_date_2026_05_04.arrays;public class MaximumProductSubarray {
+package day_039_date_2026_05_04.arrays;
+
+public class MaximumProductSubarray {
+    public static void main(String[] args) {
+        int[] nums = {2,3,-2,4};
+        System.out.println(maxProduct(nums));
+    }
+    public static int maxProductBrute(int[] nums){
+        int n = nums.length;
+        int maxProduct = nums[0];
+        int currProduct = nums[0];
+        for (int i = 1; i < n; i++) {
+            currProduct = Math.max(nums[i], nums[i]*currProduct);
+            maxProduct = Math.max(maxProduct, currProduct);
+        }
+
+        return maxProduct;
+    }
+    public static int maxProduct(int[] nums){
+        int n = nums.length;
+        int minProd = nums[0];
+        int maxProd = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            int curr = nums[i];
+
+            int tempMinProd = Math.min(curr, Math.min(curr*minProd, curr*maxProd));
+            int tempMaxProd = Math.max(curr, Math.max(curr*maxProd, curr*minProd));
+
+            minProd = tempMinProd;
+            maxProd = tempMaxProd;
+            result = Math.max(result, maxProd);
+        }
+
+        return result;
+    }
 }
