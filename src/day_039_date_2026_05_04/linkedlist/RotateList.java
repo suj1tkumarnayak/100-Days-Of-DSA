44d3b1670795caf94331c7b895539a7313777e2c
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2026-05-04 10:00:00 +0530

    Practice done on 4th May 2026

--- src/day_039_date_2026_05_04/linkedlist/RotateList.java
+++ src/day_039_date_2026_05_04/linkedlist/RotateList.java
@@ -1,2 +1,31 @@
-package day_039_date_2026_05_04.linkedlist;public class RotateList {
+package day_039_date_2026_05_04.linkedlist;
+
+import java.util.LinkedList;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+public class RotateList {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        ListNode node = new ListNode(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+//        System.out.println(rotateRight(list, 2));
+    }
+//    public static LinkedList<Integer> rotateRight(LinkedList<Integer> list, int k){
+//
+//    }
+//    public static LinkedList<Integer> reverseLinkedList(LinkedList<Integer> list){
+//
+//    }
 }
