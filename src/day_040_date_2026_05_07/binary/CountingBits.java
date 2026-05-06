b2a1f6d8a717468a8428cce7545ce54ffdf374a4
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2026-05-06 10:00:00 +0530

    Practice done on 6th May 2026

--- src/day_040_date_2026_05_07/binary/CountingBits.java
+++ src/day_040_date_2026_05_07/binary/CountingBits.java
@@ -1,2 +1,27 @@
-package day_040_date_2026_05_07.binary;public class CountingBits {
+package day_040_date_2026_05_07.binary;
+
+import java.util.Arrays;
+
+public class CountingBits {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(countBits(5)));
+    }
+    public static int[] countBits(int n){
+        int[] res = new int[n+1];
+        for (int i = 0; i < n+1; i++) {
+            res[i] = countBitsHelper(i);
+        }
+        return res;
+    }
+    public static int countBitsHelper(int n){
+        int count = 0;
+        if(n==0) return count;
+        while (n>0){
+            if(n%2==1){
+                count++;
+            }
+            n/=2;
+        }
+        return count;
+    }
 }
--- src/day_040_date_2026_05_07/binary/SumOfTwoIntegers.java
+++ src/day_040_date_2026_05_07/binary/SumOfTwoIntegers.java
@@ -1,2 +1,19 @@
-package day_040_date_2026_05_07.binary;public class SumOfTwoIntegers {
+package day_040_date_2026_05_07.binary;
+
+public class SumOfTwoIntegers {
+    public static void main(String[] args) {
+        System.out.println(toBinary(1));
+        System.out.println(toBinary(2));
+        System.out.println(toBinary(1&2));
+        //System.out.println(toBinary(5));
+    }
+    public static String toBinary(int n) {
+        if (n == 0) return "0";
+        String binary = "";
+        while (n > 0) {
+            binary = (n % 2) + binary;
+            n = n / 2;
+        }
+        return binary;
+    }
 }
