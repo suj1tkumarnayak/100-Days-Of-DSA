2492b6ba28f3cd3ca690a3fa8337b5c0afbd4c00
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2026-05-22 12:00:00 +0530

    All the practice done on 22nd may 2026

---
+++ src/day_053_date_2026_05_22/dailyleetcode/SearchInRotatedSortedArray.java
@@ -0,0 +1,13 @@
+package day_053_date_2026_05_22.dailyleetcode;
+
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        System.out.println(search(nums, 0));
+    }
+    public static int search(int[] nums, int target) {
+        int l = 0, h = nums.length-1;
+
+        return -1;
+    }
+}
--- src/day_053_date_2026_05_22/dynamicprogramming/ClimibingStairs.java
+++ src/day_053_date_2026_05_22/dynamicprogramming/ClimibingStairs.java
@@ -2,6 +2,6 @@ package day_053_date_2026_05_22.dynamicprogramming;
 
 public class ClimibingStairs {
     public static void main(String[] args) {
-        
+
     }
 }
