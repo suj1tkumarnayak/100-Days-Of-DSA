be578767b9c8faf052803a9746871309e73f0ded
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2026-06-10 12:00:00 +0530

    All the practice done on 10th June 2026

--- src/day_070_date_2026_06_09/dailyleetcode/MaximumTotalSubarrayValueI.java
+++ src/day_070_date_2026_06_09/dailyleetcode/MaximumTotalSubarrayValueI.java
@@ -1,2 +1,35 @@
-package day_070_date_2026_06_09.dailyleetcode;public class MaximumTotalSubarrayValueI {
+package day_070_date_2026_06_09.dailyleetcode;
+
+//https://leetcode.com/problems/maximum-total-subarray-value-i/description/?envType=daily-question&envId=2026-06-09
+
+public class MaximumTotalSubarrayValueI {
+
+    // Write your code inside this method
+    public static long maxTotalValue1(int[] nums, int k) {
+        return 1;
+    }
+    public static long maxTotalValue(int[] nums, int k) {
+        int max = nums[0];
+        int min = nums[0];
+
+        for (int num : nums) {
+            max = Math.max(num, max);
+            min = Math.min(num, min);
+        }
+
+
+        return 1L*k*(max-min);
+    }
+
+    public static void main(String[] args) {
+        // Test Case 1
+        int[] nums1 = {1,3,2};
+        int k1 = 2;
+        System.out.println("Test 1 Result: " + maxTotalValue(nums1, k1));
+
+        // Test Case 2
+        int[] nums2 = {4,2,5,1};
+        int k2 = 3;
+        System.out.println("Test 2 Result: " + maxTotalValue(nums2, k2));
+    }
 }
--- src/day_070_date_2026_06_09/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
+++ src/day_070_date_2026_06_09/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,2 +1,22 @@
-package day_070_date_2026_06_09.slidingwindow;public class LongestSubstringWithoutRepeatingCharacters {
+package day_070_date_2026_06_09.slidingwindow;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static int lengthOfLongestSubstring(String s) {
+        // TODO: Write your sliding window logic here
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // Test cases to check your code
+        String test1 = "abcabcbb";
+        String test2 = "bbbbb";
+        String test3 = "pwwkew";
+        String test4 = "";
+
+        System.out.println("Test 1 (Expected: 3): " + lengthOfLongestSubstring(test1));
+        System.out.println("Test 2 (Expected: 1): " + lengthOfLongestSubstring(test2));
+        System.out.println("Test 3 (Expected: 3): " + lengthOfLongestSubstring(test3));
+        System.out.println("Test 4 (Expected: 0): " + lengthOfLongestSubstring(test4));
+    }
 }
